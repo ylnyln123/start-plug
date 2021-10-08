c5144154615f1f0e0d9f6ed2562efe5fb0d9c8b7
叶立楠 <yelinan@babybus.com>
2021-10-08 18:31:02 +0800

    Fix: 新增弹窗，补充逻辑

--- extension.js
+++ extension.js
@@ -1,11 +1,7 @@
-import FileHanding from './fileHanding.js'
-
-
-
 // The module 'vscode' contains the VS Code extensibility API模块'vscode'包含VS Code扩展API  
 // Import the module and reference it with the alias vscode in your code below导入模块并使用下面代码中的别名vscode引用它  
 const vscode = require('vscode');
-
+const FileHanding = require('./fileHanding')
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -14,32 +10,46 @@ const vscode = require('vscode');
  */
 function activate(context) {
   //TODO : 在package.json中的activationEvents来定义插件被激活的条件 
-	// This line of code will only be executed once when your extension is activated这一行代码只会在你的扩展被激活时执行一次  
-	console.log('Congratulations, your extension "start-plug" is now active!');
+  // This line of code will only be executed once when your extension is activated这一行代码只会在你的扩展被激活时执行一次  
+  console.log('Congratulations, your extension "start-plug" is now active!');
   vscode.window.showInformationMessage('插件已被激活');
-	// The command has been defined in the package.json file该命令已经在包中定义。 json文件  
-	// Now provide the implementation of the command with  registerCommand现在使用registerCommand提供命令的实现  
-	// The commandId parameter must match the command field in package.json commanddid参数必须与package.json中的command字段匹配  
-	let disposable = vscode.commands.registerCommand('start-plug.helloWorld',  (params)=> {
-		// The code you place here will be executed every time your command is executed您在这里放置的代码将在每次执行命令时执行  
-		// Display a message box to the user显示消息框给用户  
-		vscode.window.showInformationMessage('Hello World from yln-plug!');
-	});
-  let removeConsole = vscode.commands.registerCommand('invalid-code-remover.removeConsole',  (params) => { 
-    console.log('!!',params);
-		vscode.window.showInformationMessage('removeConsole from yln-plug!');
-	});
-  let removeDebugger = vscode.commands.registerCommand('invalid-code-remover.removeDebugger', () => {  
-		vscode.window.showInformationMessage('removeDebugger from yln-plug!');
-	});
+  FileHanding.fileTypeJudge();
+  // The command has been defined in the package.json file该命令已经在包中定义。 json文件  
+  // Now provide the implementation of the command with  registerCommand现在使用registerCommand提供命令的实现  
+  // The commandId parameter must match the command field in package.json commanddid参数必须与package.json中的command字段匹配  
+  let disposable = vscode.commands.registerCommand('start-plug.helloWorld', () => {
 
-	context.subscriptions.push(disposable,removeConsole,removeDebugger);
+    // The code you place here will be executed every time your command is executed您在这里放置的代码将在每次执行命令时执行  
+    // Display a message box to the user显示消息框给用户  
+    vscode.window.showInformationMessage('Hello World from yln-plug!');
+  });
+  let removeConsole = vscode.commands.registerCommand('invalid-code-remover.removeConsole', (params) => {
+    console.log('!!', params);
+    vscode.window.showInputBox({
+      password: false,
+      placeHolder: '请输入需要注释的内容(忽略空格)',
+    }).then(msg => {
+      console.log('当前输入', msg);
+      const fileOptions = {
+        encoding: 'utf-8'
+      };
+      FileHanding.readFile(params.fsPath, fileOptions,msg)
+    })
+
+
+    // vscode.window.showInformationMessage('removeConsole from yln-plug!');
+  });
+  let removeDebugger = vscode.commands.registerCommand('invalid-code-remover.removeDebugger', () => {
+    vscode.window.showInformationMessage('removeDebugger from yln-plug!');
+  });
+
+  context.subscriptions.push(disposable, removeConsole, removeDebugger);
 }
 
 // this method is called when your extension is deactivated当您的扩展被停用时，将调用此方法  
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate
 }
--- fileHanding.js
+++ fileHanding.js
@@ -1,1 +1,41 @@
-export default {}
+const FileSystem = require('fs');
+function fileTypeJudge() {
+    console.log(FileSystem);  }
+function readFile(path,options,msg){
+  const fileData = FileSystem.readFileSync(path,options);
+  const fileCodeList = fileData.toString().split('\n');//将Buffer转为string类型后以行为单位分割
+  console.log('文件列表',fileCodeList);
+  handleFile(path,fileCodeList,options, msg)
+}
+function handleFile(path,fileStringList,options, msg){
+  const handleCodeList = [];
+  let isUpdate = false;
+  for (let index = 0; index < fileStringList.length; index++) {
+    const element = fileStringList[index];
+    if (isAccordCondition(element.replace(/(^\s*)/g, ""))) {//去除左边空格
+      const insertIndex = element.search('console');//TODO:先用console，后续采用动态传入
+      const handleString = `${element.slice(0,insertIndex)}// ${element.slice(insertIndex)}`;
+      handleCodeList.push(handleString);
+      isUpdate = true;
+    }else{
+      handleCodeList.push(element)
+    }
+  }
+  if (isUpdate) {//如果该文件进行了处理则重新写入
+
+    const handleFileStr = handleCodeList.join('\n');
+    FileSystem.writeFileSync(path,handleFileStr,options)
+  }
+}
+function isAccordCondition(str){
+  //TODO: 此处判断注释太过单一，后续需将/*   */考虑在内
+  if ( /^\/\//.test(str)) {//如果是注释则返回false，不做改变
+    return false
+  }
+  let exp = /^console.log/;//TODO: 后续考虑采用输入框动态传入，动态生成正则
+  return exp.test(str);
+}
+module.exports = {
+  fileTypeJudge,
+  readFile
+}
