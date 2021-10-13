fd7435d5d78d8b2dd3450c71a20af31235b282f7
叶立楠 <yelinan@babybus.com>
2021-10-13 11:38:05 +0800

    Fix： 删除无用代码，完善逻辑

--- extension.js
+++ extension.js
@@ -2,51 +2,44 @@
 // Import the module and reference it with the alias vscode in your code below导入模块并使用下面代码中的别名vscode引用它  
 const vscode = require('vscode');
 const FileHanding = require('./fileHanding')
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
   //TODO : 在package.json中的activationEvents来定义插件被激活的条件 
-  // This line of code will only be executed once when your extension is activated这一行代码只会在你的扩展被激活时执行一次  
+  // 这一行代码只会在你的扩展被激活时执行一次  
   console.log('Congratulations, your extension "start-plug" is now active!');
   vscode.window.showInformationMessage('插件已被激活');
-  FileHanding.fileTypeJudge();
-  // The command has been defined in the package.json file该命令已经在包中定义。 json文件  
-  // Now provide the implementation of the command with  registerCommand现在使用registerCommand提供命令的实现  
-  // The commandId parameter must match the command field in package.json commanddid参数必须与package.json中的command字段匹配  
+  // 该命令已经在包中定义。 json文件  
+  // 现在使用registerCommand提供命令的实现  
+  // 参数必须与package.json中的command字段匹配  
   let disposable = vscode.commands.registerCommand('start-plug.helloWorld', () => {
-
-    // The code you place here will be executed every time your command is executed您在这里放置的代码将在每次执行命令时执行  
-    // Display a message box to the user显示消息框给用户  
-    vscode.window.showInformationMessage('Hello World from yln-plug!');
+    
+    
   });
-  let removeConsole = vscode.commands.registerCommand('invalid-code-remover.removeConsole', (params) => {
-    console.log('!!', params);
+  let codeClear = vscode.commands.registerCommand('custom-code.clear', (params) => {
     vscode.window.showInputBox({
       password: false,
-      placeHolder: '请输入需要注释的内容(忽略空格)',
+      placeHolder: '请输入需要将其注释的内容(逐行匹配，匹配开头，忽略空格)',
     }).then(msg => {
-      console.log('当前输入', msg);
-      const fileOptions = {
-        encoding: 'utf-8'
-      };
-      FileHanding.readFile(params.fsPath, fileOptions,msg)
+      if (msg) {
+        const fileOptions = {
+          encoding: 'utf-8'
+        };
+        FileHanding.handleMain(params.fsPath, fileOptions,`${msg}`).then(res => {
+          vscode.window.showInformationMessage('文件已更新，可以进行查看咯');
+        },err => {
+          vscode.window.showErrorMessage(err)
+        })
+      }
     })
-
-
-    // vscode.window.showInformationMessage('removeConsole from yln-plug!');
-  });
-  let removeDebugger = vscode.commands.registerCommand('invalid-code-remover.removeDebugger', () => {
-    vscode.window.showInformationMessage('removeDebugger from yln-plug!');
   });
 
-  context.subscriptions.push(disposable, removeConsole, removeDebugger);
+  context.subscriptions.push(disposable, codeClear);
 }
 
-// this method is called when your extension is deactivated当您的扩展被停用时，将调用此方法  
+// 当您的扩展被停用时，将调用此方法  
 function deactivate() {}
 
 module.exports = {
--- fileHanding.js
+++ fileHanding.js
@@ -1,41 +1,87 @@
+//TODO:想做一个类似日志的txt文件，用来保存每次更改的记录，可是面对mac和windows的目录差异，如何确定放置位置？
+// TODO: 考虑是否要做文件类型筛选
+
 const FileSystem = require('fs');
-function fileTypeJudge() {
-    console.log(FileSystem);  }
-function readFile(path,options,msg){
-  const fileData = FileSystem.readFileSync(path,options);
-  const fileCodeList = fileData.toString().split('\n');//将Buffer转为string类型后以行为单位分割
-  console.log('文件列表',fileCodeList);
-  handleFile(path,fileCodeList,options, msg)
+
+function isFolder(path) { //判断是否是文件夹
+  return FileSystem.lstatSync(path).isDirectory();
 }
-function handleFile(path,fileStringList,options, msg){
+function handleMain(path, options, msg){
+  return new Promise((resolve,reject) => {
+    if (isFolder(path)) {//如果是文件夹，开始递归
+      const filePathList = FileSystem.readdirSync(path);
+      console.log('文件列表',filePathList,path);
+      if (filePathList.length) {
+        for (let index = 0; index < filePathList.length; index++) {
+          const childPath = `${path}/${filePathList[index]}`;
+          handleMain(childPath,options, msg)
+        }
+      }else{
+        reject('文件夹为空')
+      }
+    }else{
+      const result = readFile(path, options, msg)
+      result.state ? resolve() : reject(result.errCode);
+    }
+  })
+}
+function readFile(path, options, msg) {
+    if (!path || !msg) {
+      return {
+        state: false,
+        errCode: '当前状态异常'
+      }
+    }
+    if (!FileSystem.existsSync(path)) {// 判断文件是否存在
+      return {
+        state: false,
+        errCode: '现有路径没有找到文件'
+      }
+    }
+    try {
+      const fileData = FileSystem.readFileSync(path, options);
+      const fileCodeList = fileData.toString().split('\n'); //将Buffer转为string类型后以行为单位分割
+      clearHandleFile(path, fileCodeList, options, msg); // 路径、代码行数组、字符类型、用户输入文本
+      return {
+        state: true
+      }
+    } catch (error) {
+      return {
+        state: false,
+        errCode: `未知错误${error}`
+      }
+    }
+}
+
+function clearHandleFile(path, fileStringList, options, msg) {
   const handleCodeList = [];
   let isUpdate = false;
   for (let index = 0; index < fileStringList.length; index++) {
     const element = fileStringList[index];
-    if (isAccordCondition(element.replace(/(^\s*)/g, ""))) {//去除左边空格
-      const insertIndex = element.search('console');//TODO:先用console，后续采用动态传入
+    if (isAccordCondition(element.replace(/(^\s*)/g, ""), msg)) { //去除左边空格
+      const insertIndex = element.search(msg);
       const handleString = `${element.slice(0,insertIndex)}// ${element.slice(insertIndex)}`;
       handleCodeList.push(handleString);
       isUpdate = true;
-    }else{
+    } else {
       handleCodeList.push(element)
     }
   }
-  if (isUpdate) {//如果该文件进行了处理则重新写入
-
+  if (isUpdate) { //如果该文件进行了处理则重新写入，没有变更的话不写入
     const handleFileStr = handleCodeList.join('\n');
-    FileSystem.writeFileSync(path,handleFileStr,options)
+    FileSystem.writeFileSync(path, handleFileStr, options);
   }
 }
-function isAccordCondition(str){
+
+function isAccordCondition(str, msg) {
   //TODO: 此处判断注释太过单一，后续需将/*   */考虑在内
-  if ( /^\/\//.test(str)) {//如果是注释则返回false，不做改变
+  if (/^\/\//.test(str)) { //如果是注释则返回false，不做改变
     return false
   }
-  let exp = /^console.log/;//TODO: 后续考虑采用输入框动态传入，动态生成正则
+  let exp = RegExp(`^${msg}`); //采用输入框动态传入，匹配开头
   return exp.test(str);
 }
 module.exports = {
-  fileTypeJudge,
-  readFile
+  readFile,
+  handleMain
 }
